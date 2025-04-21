a3b0e81afd5d91998b5f85859d399788adc90e10
Ivan Onofre <onofreivan100@gmail.com>
2025-04-20 21:22:39 -0700

    Create the sign-up form

---
+++ core/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class SignUpForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Your username',
+        'class': 'w-full py-4 px-6 rounded-xl bg-white',
+    }))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
+        'placeholder': 'Your email address',
+        'class': 'w-full py-4 px-6 rounded-xl bg-white',
+    }))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Your password',
+        'class': 'w-full py-4 px-6 rounded-xl bg-white',
+    }))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Repeat password',
+        'class': 'w-full py-4 px-6 rounded-xl bg-white',
+    }))
--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
+from .forms import SignUpForm
 from item.models import Category, Item
 
 # Create your views here.
@@ -14,3 +15,16 @@ def index(request):
 
 def contact(request):
     return render(request, 'core/contact.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            return redirect('/login/')
+    else:
+        form = SignUpForm()
+    return render(request, 'core/signup.html', {
+        'form': form,
+    })
